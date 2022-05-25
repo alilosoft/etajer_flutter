c01d2423c48dbb3a76061b3d14d3a1c36dcc0bcf
Ali Fellahi <fellahi.ali@gmail.com>
2022-05-25 15:31:20 +0100

    make sure DB started correctly
    by starting docker in attached mode and parse the log

--- bin/docker_run.sh
+++ bin/docker_run.sh
@@ -1,15 +1,12 @@
 #!bin/bash
 
-echo running docker...
+echo starting postgres container...
 
-# we need to start docker in detached mode (-d), 
-# to run the process in the background, 
-# and allow our program to continue it's execution, mainly runing tests 
-    #--rm \
+# start container in attached mode and parse the log to determine if the database started correctly
 docker run \
-    -d \
+    --rm \
     -p 5432:5432 \
     -e POSTGRES_USER=test \
     -e POSTGRES_PASSWORD=test \
     -e POSTGRES_DB=test_db \
-    postgres:14-alpine \
+    postgres:14 \
--- bin/playground.dart
+++ bin/playground.dart
@@ -1,18 +1,26 @@
+import 'dart:convert';
 import 'dart:io';
 
 void main() async {
-  final containerID = await startContainer();
-  print('the end 😎 $containerID');
+  final started = await startContainer();
+  print('container started: $started ${started ? '😎' : '😥'}');
 }
 
-startContainer() async {
+Future<bool> startContainer() async {
   final p = await Process.start(
     'bash',
     ['docker_run.sh'],
     workingDirectory: './bin',
   );
   print('docker process pid: ${p.pid}');
-  stderr.addStream(p.stderr);
-  await stdout.addStream(p.stdout);
-  return p.pid;
+
+  final r = p.stderr.any((e) {
+    final output = utf8.decode(e);
+    stdout.writeln(output);
+    if (output.contains('database system is ready to accept connections')) {
+      return true;
+    }
+    return false;
+  });
+  return r;
 }
