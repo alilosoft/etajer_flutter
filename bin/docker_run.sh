8790cf76b8d5f5ff72788082564c38318c7749d3
Ali Fellahi <fellahi.ali@gmail.com>
2022-05-25 18:59:11 +0100

    allow passing container name to script

--- bin/docker_run.sh
+++ bin/docker_run.sh
@@ -1,9 +1,12 @@
 #!bin/bash
 
-echo starting postgres container...
+echo 'stopping container named '$1' if exist'
+docker stop $1
 
+echo 'starting postgres container named: '$1' ...'
 # start container in attached mode and parse the log to determine if the database started correctly
 docker run \
+    --name $1 \
     --rm \
     -p 5432:5432 \
     -e POSTGRES_USER=test \
